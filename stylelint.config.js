9d8ba83b1f9b64c2bbe0b36a159c68a84cbd0526
suzukey <suzukei0208@gmail.com>
2020-11-02 13:48:12 +0900

    🚨 コメント前の改行なしを許可

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,5 +2,8 @@ module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
-  rules: {},
+  rules: {
+    // コメントの前に改行はいらない
+    'comment-empty-line-before': null,
+  },
 }
