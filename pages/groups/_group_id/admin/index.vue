4a77aa38d448b7951a7a593b7cccea51877dbc08
suzukey <suzukei0208@gmail.com>
2020-11-13 20:01:41 +0900

    💄 管理メニュー

--- pages/groups/_group_id/admin/index.vue
+++ pages/groups/_group_id/admin/index.vue
@@ -1,135 +1,98 @@
 <template>
-  <div class="top">
-    <div class="content">
-      <main>
-        <div class="bara1"></div>
-        <div class="wrapper">
-          <a href="/survey" rel="noopener noreferrer" class="one">
-            <div class="btnsize" size="lg" squared variant="outline-secondary">
-              希望調査
-            </div>
-          </a>
-
-          <a href="/shift" rel="noopener noreferrer" class="one">
-            <div class="btnsize" size="lg" squared variant="outline-secondary">
-              シフト作成
-            </div>
-          </a>
-
-          <a href="/survey" rel="noopener noreferrer" class="one">
-            <div class="btnsize" size="lg" squared variant="outline-secondary">
-              希望閲覧
-            </div>
-          </a>
-        </div>
-        <div class="bara2"></div>
-        <div class="wrapper">
-          <a href="/survey" rel="noopener noreferrer" class="one">
-            <div class="btnsize" size="lg" squared variant="outline-secondary">
-              権限付与
-            </div>
-          </a>
-          <a href="/survey" rel="noopener noreferrer" class="one">
-            <div class="btnsize" size="lg" squared variant="outline-secondary">
-              メンバー
-            </div>
-          </a>
-          <a href="/survey" rel="noopener noreferrer" class="one">
-            <div class="btnsize" size="lg" squared variant="outline-secondary">
-              グループ
-            </div>
-          </a>
-        </div>
-      </main>
+  <div class="admin">
+    <div class="main-header">
+      <div class="page-title">
+        管理メニュー
+      </div>
+    </div>
+    <div class="links">
+      <div class="link">
+        <nuxt-link :to="`/groups/${groupId}/admin/surveys`">
+          希望調査
+        </nuxt-link>
+      </div>
+      <div class="link">
+        <nuxt-link :to="`/groups/${groupId}/admin/shifts`">
+          シフト作成
+        </nuxt-link>
+      </div>
+      <div class="link">
+        <nuxt-link :to="`/groups/${groupId}/admin/wishes`">
+          希望閲覧
+        </nuxt-link>
+      </div>
+      <div class="link">
+        <nuxt-link :to="`/groups/${groupId}/admin/roles`">
+          権限
+        </nuxt-link>
+      </div>
+      <div class="link">
+        <nuxt-link :to="`/groups/${groupId}/admin/members`">
+          メンバー
+        </nuxt-link>
+      </div>
+      <div class="link">
+        <nuxt-link :to="`/groups/${groupId}/admin/group`">
+          グループ
+        </nuxt-link>
+      </div>
     </div>
   </div>
 </template>
 <script>
 export default {
   layout: 'group',
+  computed: {
+    groupId() {
+      return this.$route.params.group_id
+    },
+  },
 }
 </script>
 
 <style scoped>
-.content {
+.main-header {
+  align-items: center;
+  background-color: #00897b;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+  color: #fff;
   display: flex;
-  height: calc(100vh - 70px);
+  height: 65px;
+  padding: 0 30px;
+  position: relative;
+  z-index: 3;
 }
 
-.list-group-item {
-  height: 75px;
+.main-header .page-title {
+  font-size: 20px;
 }
 
-.sidemenu {
-  height: 100%;
-  width: 350px;
-  background-color: #9b9b9b;
-}
-
-.sidemenu-content {
-  height: calc(100% - 76px);
-}
-
-.sidemenu-content .profile {
-  border-bottom: 1px solid #9b9b9b;
-}
-
-.sidemenu-content .profile .list-group-item {
-  height: 100px;
-}
-
-.sidemenu-content .groups {
-  height: 30px;
-  color: #fff;
-  background-color: #616161;
-}
-.sidemenu-content .groups1 {
-  display: block;
-  color: #fff;
-  font-size: 30px;
-  height: 60px;
-  width: 100%;
-  background-color: #373737;
-}
-
-main {
-  width: 100%;
-}
-
-/* ボタン配置と大きさなどの設定 */
-
-.wrapper {
-  width: 640px;
+.links {
+  color: #616161;
   display: flex;
   flex-wrap: wrap;
+  font-size: 20px;
+  font-weight: 700;
   justify-content: space-between;
-  margin: auto;
-  font-size: 1.5rem;
+  padding: 30px;
 }
 
-.one {
-  box-sizing: border-box;
-  border: 2px solid #999;
-  width: 200px;
-  height: 200px;
-  display: flex;
+.links .link {
   align-items: center;
+  display: flex;
   justify-content: center;
-  color: #000;
+  width: calc(100% / 3);
+}
 
-  /* 下線なし */
+.links a {
+  align-items: center;
+  box-shadow: 0 1px 3px 1px rgba(0, 0, 0, 0.2);
+  color: inherit;
+  display: flex;
+  height: 135px;
+  justify-content: center;
+  margin-top: 45px;
   text-decoration: none;
-}
-.bara1 {
-  width: 100%;
-  height: 200px;
-}
-.bara2 {
-  width: 100%;
-  height: 50px;
-}
-.wrapper a:hover {
-  background-color: rgb(207, 207, 207);
-  color: #fff;
+  width: 240px;
 }
 </style>
