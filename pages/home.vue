c0cc0b02b4a820727acf2f7dae425a3c6f1b8742
suzukey <suzukei0208@gmail.com>
2020-11-13 23:50:10 +0900

    🚧 ホームのカレンダーを変えています

--- pages/home.vue
+++ pages/home.vue
@@ -1,13 +1,40 @@
 <template>
   <div class="home">
-    <b-calendar
-      selected-variant="success"
-      today-variant="info"
-      nav-button-variant="primary"
-      hide-header
-      width="100%"
-    >
-    </b-calendar>
+    <div class="main-header">
+      <div class="center-actions">
+        <div class="arrow">
+          <i class="mdi mdi-chevron-left" />
+        </div>
+        <div class="calendar-month">
+          2020年 11月
+        </div>
+        <div class="arrow">
+          <i class="mdi mdi-chevron-right" />
+        </div>
+      </div>
+      <div class="print">
+        <i class="mdi mdi-printer" />
+      </div>
+    </div>
+    <div class="calendar">
+      <div class="day-of-week">
+        <div class="sun">日</div>
+        <div class="mon">月</div>
+        <div class="tue">火</div>
+        <div class="wed">水</div>
+        <div class="thu">木</div>
+        <div class="fri">金</div>
+        <div class="sat">土</div>
+      </div>
+      <div class="weeks">
+        <div class="week"></div>
+        <div class="week"></div>
+        <div class="week"></div>
+        <div class="week"></div>
+        <div class="week"></div>
+        <div class="week"></div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,34 +44,81 @@ export default {
 }
 </script>
 
-<style>
-.b-calendar {
-  width: 100%;
-  height: calc(100vh - 70px);
+<style scoped>
+.main-header {
+  align-items: center;
+  background-color: #00897b;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+  color: #fff;
+  display: flex;
+  height: 65px;
+  padding: 0 30px;
+  position: relative;
+  z-index: 3;
 }
 
-.b-calendar .b-calendar-grid {
-  height: calc(100% - 60px) !important;
+.main-header .center-actions {
+  align-items: center;
+  display: flex;
+  flex: 1;
+  font-size: 20px;
+  justify-content: center;
 }
 
-.b-calendar .row {
-  height: calc(calc(100vh - 160px) / 6) !important;
+.main-header .center-actions .calendar-month {
+  margin: 0 75px;
 }
 
-.b-calendar .col {
-  border: 1px solid #cacaca;
+.main-header .arrow,
+.main-header .print {
+  font-size: 24px;
+  transition: 300ms;
 }
 
-.b-calendar .btn {
-  width: 50px !important;
-  height: 50px !important;
+.main-header .arrow {
+  align-items: center;
+  display: flex;
+  height: 35px;
+  justify-content: center;
+  width: 70px;
 }
 
-.b-calendar span {
-  font-size: 30px !important;
+.main-header .print {
+  align-items: center;
+  border-radius: 50%;
+  display: flex;
+  height: 50px;
+  justify-content: center;
+  width: 50px;
 }
 
-.b-calendar footer {
-  display: none !important;
+.main-header .arrow:hover,
+.main-header .print:hover {
+  background-color: rgba(0, 0, 0, 0.1);
+}
+
+.calendar {
+  height: 100vh;
+  max-height: calc(100vh - 70px - 65px);
+}
+
+.calendar .day-of-week {
+  align-items: center;
+  color: #8e8e8e;
+  border-bottom: 2px solid #cfcfcf;
+  display: flex;
+  font-size: 14px;
+  font-weight: 700;
+  height: 40px;
+  justify-content: space-around;
+}
+
+.calendar .weeks {
+  height: calc(100% - 40px);
+}
+
+.calendar .week {
+  border-bottom: 2px solid #cfcfcf;
+  height: calc(100% / 6);
 }
 </style>
