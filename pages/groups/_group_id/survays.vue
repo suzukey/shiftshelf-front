42251dbf2db8df84af7157371abd2cee7145ad72
unknown <watamelon.watawata@gmail.com>
2020-11-19 12:03:56 +0900

    一般シフト募集一覧画面の追加

---
+++ pages/groups/_group_id/survays.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div v-for="post in posts" :key="post">
+      <nuxt-link :to="`/groups/1/survays/${post.shift_wish_id}`">{{
+        post.shift_wish_name
+      }}</nuxt-link>
+    </div>
+    <!-- <div class="calendar-canvas">
+      <FullCalendar :target="target" />
+    </div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'group',
+  async asyncData({ $axios, $dayjs }) {
+    const posts = await $axios.$get('/v1/groups/1/posts')
+    return { posts, target: $dayjs() }
+  },
+}
+</script>
+<style scoped>
+.calendar-canvas {
+  height: 100vh;
+  max-height: calc(100vh - 70px - 65px);
+}
+</style>
