1fc77ff976003c87bf4c2e2d32b86f54a171da55
suzukey <suzukei0208@gmail.com>
2020-11-21 17:32:49 +0900

    💄 スマホサイズお試し

---
+++ pages/size-test.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="base">
+    <h1>{{ wh }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    wh() {
+      return window.innerHeight
+    },
+  },
+}
+</script>
+
+<style scoped>
+.base {
+  align-items: center;
+  background-color: #00897b;
+  color: #fff;
+  display: flex;
+  height: 100vh;
+  height: calc(var(--wh, 1vh) * 100);
+  justify-content: center;
+  position: relative;
+  width: 100%;
+}
+</style>
