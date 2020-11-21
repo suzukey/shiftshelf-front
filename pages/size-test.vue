c4a8b23562c769c5b10ef0cb7dcdf42f5e24ee0f
suzukey <suzukei0208@gmail.com>
2020-11-21 17:42:35 +0900

    💄 100%継承パターン

--- pages/size-test.vue
+++ pages/size-test.vue
@@ -5,14 +5,28 @@
 </template>
 
 <script>
+import windowSize from '~/mixins/windowSize.js'
+
 export default {
   computed: {
     wh() {
       return window.innerHeight
     },
   },
+  mixins: [windowSize],
 }
 </script>
+
+<style>
+html,
+body,
+#__nuxt,
+#__layout,
+#__layout > div {
+  width: 100%;
+  height: 100%;
+}
+</style>
 
 <style scoped>
 .base {
@@ -20,8 +34,7 @@ export default {
   background-color: #00897b;
   color: #fff;
   display: flex;
-  height: 100vh;
-  height: calc(var(--wh, 1vh) * 100);
+  height: 100%;
   justify-content: center;
   position: relative;
   width: 100%;
