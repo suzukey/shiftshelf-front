3d8d5532630c406fd3bf835a61b839750d5c9b9f
suzukey <suzukei0208@gmail.com>
2020-12-07 00:09:50 +0900

    lighthouseのパフォーマンスを改善させます

--- nuxt.config.js
+++ nuxt.config.js
@@ -73,9 +73,7 @@ export default {
    ** Build configuration
    ** See https://nuxtjs.org/api/configuration-build/
    */
-  build: {
-    extractCSS: process.env.NODE_ENV === 'production',
-  },
+  build: {},
   generate: {
     fallback: true,
   },
