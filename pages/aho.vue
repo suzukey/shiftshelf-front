61e039ee60064544c14944f5829a7d42a0a0f7c1
suzukey <suzukei0208@gmail.com>
2020-12-09 11:54:05 +0900

    warningが出ないように変更

--- pages/aho.vue
+++ pages/aho.vue
@@ -4,11 +4,11 @@
       <template v-for="tr in rows">
         <tr :key="tr.index">
           <template v-for="cell in tr.table_cells">
-            <th :key="cell.index" v-if="cell.cell_type == 'TH'">
+            <th v-if="cell.cell_type == 'TH'" :key="cell.index">
               <input type="text" :value="cell.cell_type" />
             </th>
 
-            <td :key="cell.index" v-else-if="cell.cell_type == 'TD'">
+            <td v-else-if="cell.cell_type == 'TD'" :key="cell.index">
               <input type="text" />
             </td>
           </template>
